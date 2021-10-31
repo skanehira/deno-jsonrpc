a5353425d94d009b8a86ede0cc3396595aa24094
skanehira <sho19921005@gmail.com>
2021-10-31 18:04:23 +0900

    first implement

--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
 export * from "https://deno.land/std@0.113.0/testing/asserts.ts";
+export * as io from "https://deno.land/std@0.113.0/io/mod.ts";
--- mod.ts
+++ mod.ts
@@ -1,1 +1,68 @@
-export const add = (a: number, b: number) => (a + b);
+import { io } from "./deps.ts";
+
+export interface Request {
+  jsonrpc?: "2.0";
+  id?: number;
+  method: string;
+  params?: Record<string, unknown> | Array<Record<string, unknown>>;
+}
+
+export interface RPCError {
+  code: number;
+  message: string;
+  data?: unknown;
+}
+
+export interface Response<T> {
+  readonly jsonrpc: "2.0";
+  result?: T;
+  error?: RPCError;
+}
+
+export class Client {
+  #id: number;
+  conn: Deno.Conn;
+  encoder: TextEncoder;
+  decoder: TextDecoder;
+
+  constructor(conn: Deno.Conn) {
+    this.conn = conn;
+    this.#id = 0;
+    this.encoder = new TextEncoder();
+    this.decoder = new TextDecoder();
+  }
+
+  private async readResponse<T>(): Promise<Response<T>> {
+    const cap = 128;
+    const p = new Uint8Array(cap);
+    const buf = new io.Buffer();
+    while (1) {
+      const readed = await this.conn.read(p);
+      if (readed === null || readed === 0) { // EOF
+        break;
+      }
+      if (readed !== cap) {
+        buf.write(p.slice(0, readed));
+        break;
+      }
+      buf.write(p);
+    }
+
+    const body = this.decoder.decode(buf.bytes());
+    return JSON.parse(body) as Response<T>;
+  }
+
+  async Request<T>(req: Request): Promise<Response<T>> {
+    req.id = this.#id;
+    req.jsonrpc = "2.0";
+    const body = JSON.stringify(req);
+    console.log(body);
+    await this.conn.write(this.encoder.encode(body));
+    this.#id++;
+    return this.readResponse();
+  }
+
+  close() {
+    this.conn.close();
+  }
+}
